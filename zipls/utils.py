3f511b6acaa8eb6b25a2b865e8bfd0c6895149ca
Ljzd-PRO <63289359+Ljzd-PRO@users.noreply.github.com>
2024-05-01 22:13:23 +0800

    feat: support rar

--- zipls/utils.py
+++ zipls/utils.py
@@ -2,7 +2,9 @@ from datetime import datetime
 from glob import glob
 from pathlib import Path
 from typing import Generator, Any
-from zipfile import ZipFile
+from zipfile import ZipFile, BadZipFile
+
+from rarfile import RarFile
 
 from zipls.model import ZipLsInfo, FileInfo
 
@@ -13,15 +15,26 @@ def dump_zipls_info(path: Path) -> ZipLsInfo:
     :param path: Zip file path.
     """
     zipls_info = ZipLsInfo(path=path, size=path.lstat().st_size, files=[])
-    zipfile = ZipFile(path)
-    for sub_file in zipfile.filelist:
-        zipls_info.files.append(
-            FileInfo(
-                name=sub_file.filename,
-                size=sub_file.file_size,
-                date=datetime(*sub_file.date_time)
+    try:
+        zipfile = ZipFile(path)
+        for sub_file in zipfile.filelist:
+            zipls_info.files.append(
+                FileInfo(
+                    name=sub_file.filename,
+                    size=sub_file.file_size,
+                    date=datetime(*sub_file.date_time)
+                )
             )
-        )
+    except BadZipFile:
+        rarfile = RarFile(path)
+        for sub_file in rarfile:
+            zipls_info.files.append(
+                FileInfo(
+                    name=sub_file.filename,
+                    size=sub_file.file_size,
+                    date=datetime(*sub_file.date_time)
+                )
+            )
     return zipls_info
 
 
